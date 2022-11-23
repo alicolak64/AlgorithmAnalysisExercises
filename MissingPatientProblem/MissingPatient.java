fce0848beb523dea4964e5b1f7e763af35ff6f10
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-23 11:15:28 +0300

    Fixed codes

--- MissingPatientProblem/MissingPatient.java
+++ MissingPatientProblem/MissingPatient.java
@@ -12,24 +12,22 @@ public class MissingPatient {
 
         final int M = N - 1;
 
-        int[] patients = new int[M];
+        final int[] PATIENTS = new int[M];
 
         // K = {  10K  ,  20K  ,  30K  ,  40K  ,  50K  ,  60K  ,  70K  ,  80K  ,  90K  ,  100K  ,  200K  ,  300K  ,  400K  ,  500K  ,  1000K  }
         // K = { 10000 , 20000 , 30000 , 40000 , 50000 , 60000 , 70000 , 80000 , 90000 , 100000 , 200000 , 300000 , 400000 , 500000 , 1000000 }
 
-
-        int missingPatient = fillArray(patients);
+        int missingPatient = fillArray(PATIENTS);
 
         System.out.println("Missing Patient : " + missingPatient);
 
         long startTime = getTime();
-        int foundMissingPatient = getMissingPatient(patients, 0, patients.length - 1);
+        int foundedMissingPatient = getMissingPatient(PATIENTS, 0, PATIENTS.length - 1);
         long endTime = getTime();
-        System.out.println("Founded Patient : " + foundMissingPatient);
+        System.out.println("Founded Patient : " + foundedMissingPatient);
 
         System.out.print("Array size = " + (N / 1000) + "K" + " , ");
-        System.out.println("Time : " + timeDifference(startTime, endTime) + "ms");
-
+        System.out.println("Time : " + getTimeDifference(startTime, endTime) + "ms");
 
     }
 
@@ -51,9 +49,7 @@ public class MissingPatient {
 
         for (int i = 0; i < array.length; i++) {
             int randomIndexToSwap = random.nextInt(array.length);
-            int temp = array[randomIndexToSwap];
-            array[randomIndexToSwap] = array[i];
-            array[i] = temp;
+            swap(array, randomIndexToSwap, i);
         }
 
         return randomNumber;
@@ -77,37 +73,35 @@ public class MissingPatient {
     }
 
     // It takes the difference of the nanosecond type time values and converts the result to ms.
-    public static double timeDifference(long startTime, long finishTime) {
+    public static double getTimeDifference(long startTime, long finishTime) {
         return (finishTime - startTime) / 1000000.0;
     }
 
 
     // return the missing patient's number
-    public static int getMissingPatient(int[] arr, int low, int high) {
+    public static int getMissingPatient(int[] array, int low, int high) {
 
-        if ( low >= high )
-            return arr[low] - 1;
+        if (low >= high)
+            return array[low] - 1;
 
-        int[] midElement = getMidElement(arr, low, high);
+        int avgIndex = getAverageIndex(array, low, high);
 
-        if (midElement[0] < 0)
-            return midElement[1];
+        if (avgIndex < 0)  // avg is not in the array so avg is missing patient
+            return avgIndex * -1;
 
-        arr[midElement[0]] = arr[low];
-        arr[low] = midElement[1];
-
-        int pivot = partition(arr, low, high);
+        swap(array, low, avgIndex);  // pivot that avg value assign to the first element
+        int pivot = partition(array, low, high);
 
         // T(n)=T(n/2)+Θ(n).
-        if (arr[midElement[1] - 1] == pivot)
-            return getMissingPatient(arr, midElement[1], high);
-        else
-            return getMissingPatient(arr, 0, midElement[1] - 2);
+        if (array[pivot - 1] == pivot) // missing patient on the right side of the pivot
+            return getMissingPatient(array, pivot, high);
+        else // missing patient on the left side of the pivot
+            return getMissingPatient(array, 0, pivot - 2);
 
     }
 
     // Partition method with pivot value first element and change operation with pivot
-    public static int partition(int[] arr, int low, int high) {
+    private static int partition(int[] arr, int low, int high) {
 
         int pivot = arr[low];
 
@@ -116,51 +110,55 @@ public class MissingPatient {
 
         while (i < j) {
 
-            while (i < j && arr[j] >= pivot) {
+            while (arr[j] >= pivot && i < j)
                 j--;
-            }
 
-            if (i < j) {
-                arr[i] = arr[j];
-                arr[j] = pivot;
-            }
+            if (i < j)
+                swap(arr, i, j);
 
-            while (i < j && arr[i] <= pivot) {
+            while (arr[i] <= pivot && i < j)
                 i++;
-            }
 
-            if (i < j) {
-                arr[j] = arr[i];
-                arr[i] = pivot;
-            }
+            if (i < j)
+                swap(arr, i, j);
 
         }
 
         return pivot;
 
-
     }
 
 
     // It takes the average of the elements in the array, and if the element with this average is in the array, it gives the average directly, if it is not in the array, it multiplies the average by (-1) and returns that value.
-    public static int[] getMidElement(int[] array, int low, int high) {
+    private static int getAverageIndex(int[] array, int low, int high) {
 
-        int midIndex = -1;
+        int average = ((high - low) / 2) + 1 + low;
 
-        long average = ((high - low) / 2) + 1 + low;
+        int avgIndex = getIndex(array, average, low, high);
 
-        for (int i = low; i <= high; i++) {
-            if (array[i] == average) {
-                midIndex = i;
-                break;
-            }
-        }
-
-        if (midIndex == -1)
-            return new int[]{-1, (int) average};   // Missing patient is average value So return -1 index
+        if (avgIndex == -1)
+            return -1 * average;   // Missing patient number is average value So return -1 index
         else
-            return new int[]{midIndex, (int) average};
+            return avgIndex;
 
+    }
+
+    // get index of value in array between low and high indexes
+    private static int getIndex(int[] array, int value, int low, int high) {
+
+        for (int i = low; i <= high; i++)
+            if (array[i] == value)
+                return i;
+
+        return -1;
+
+    }
+
+    // swap method
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
